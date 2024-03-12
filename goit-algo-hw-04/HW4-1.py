a38912f861869141fd95136961365ae0f6c84a00
nickkyiv <159835975+nickkyiv@users.noreply.github.com>
2024-03-12 21:15:23 +0000

    modified:   goit-algo-hw-04/HW4-1.py

--- goit-algo-hw-04/HW4-1.py
+++ goit-algo-hw-04/HW4-1.py
@@ -1,13 +1,26 @@
+# Вміст імпортованого файлу:
+# Alex Korp,3000
+# Mykyta Borysenko,2000
+# Sitarama Raju,1000
+# Jack Jones,1500
+# Duke Nukem, 1750
+# Сума: 9250, середнє: 1850
+
 def total_salary(path):
     total_s = 0 # Total salary
     times = 0 # Counter of salaries/workers
-    with open(path, "r", encoding="utf-8") as file:
-        for item in file.readlines():
-            _, salary = item.split(",")
-            total_s += int(salary) # Total salary calculation
-            times += 1
-    average_s = total_s / times # Average salary calculation   
-    return (total_s, average_s)
-
-print(total_salary('goit-algo-hw-04/hw4-1-salaries.txt'))
-
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for item in file.readlines():
+                _, salary = item.split(",")
+                total_s += int(salary) # Total salary calculation
+                times += 1
+        average_s = total_s / times # Average salary calculation   
+        return (total_s, average_s)
+    except (FileNotFoundError, ValueError):
+        print("Sorry, missing file or wrong file format.")
+try:
+    total, average = total_salary('goit-algo-hw-04/hw4-1-salaries.txt')
+    print(f"Загальна сума заробітної плати: {total}, середня заробітна плата: {average}")
+except TypeError:
+    pass
