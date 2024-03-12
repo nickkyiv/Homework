ad81768f710a2ecd4c12ceddbcb7f6ecf464f23c
nickkyiv <159835975+nickkyiv@users.noreply.github.com>
2024-03-12 21:50:26 +0000

    modified:   goit-algo-hw-04/HW4-2.py 	new file:   goit-algo-hw-04/cats_info.txt

---
+++ goit-algo-hw-04/HW4-2.py
@@ -0,0 +1,14 @@
+def get_cats_info(path):
+    list = []
+    try:
+        with open(path, "r", encoding="utf-8") as file:
+            for item in file.readlines():
+                id, name, age = item.strip().split(",")
+                dic = {"id": id, "name": name, "age": age}
+                list.append(dic)
+        return (list)
+    except (FileNotFoundError, ValueError):
+        print("Sorry, missing file or wrong file format.")
+    
+cats_info = get_cats_info("goit-algo-hw-04\cats_info.txt")
+print(cats_info)
