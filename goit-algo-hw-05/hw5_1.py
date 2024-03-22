581badbcd3b9c979c8d6ecc932562650a72d0e96
nickkyiv <159835975+nickkyiv@users.noreply.github.com>
2024-03-22 18:00:10 +0000

    modified:   goit-algo-hw-05/hw5_1.py

--- goit-algo-hw-05/hw5_1.py
+++ goit-algo-hw-05/hw5_1.py
@@ -6,17 +6,18 @@ def caching_fibonacci(n: int, cache={}):
         if n == 1:
             return 1
         if n in cache:
-            print(f"Using cache for the value {n}")
+            print(f"Using cache for the value {n}") # Для демонстрації використання кешу
             return cache[n]
         
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
-        print(cache)
+        print(f"writing {n} to cache:", cache) # Для демонстрації запису й наявності кешу
         return cache[n]
         
     return fibonacci(n)
 
 fibonacci = caching_fibonacci
-print(f"Число Фібоначчі для 14 дорівнює {fibonacci(14)}")
-print(f"Число Фібоначчі для 10 дорівнює {fibonacci(10)}")
+print(f"Число Фібоначчі для 14 дорівнює {fibonacci(14)}") # Розрахунок ведеться лише тут, 
+print(f"Число Фібоначчі для 10 дорівнює {fibonacci(10)}") # у наступних принтах дані беруться з кешу
 print(f"Число Фібоначчі для 11 дорівнює {fibonacci(11)}")
 print(f"Число Фібоначчі для 7 дорівнює {fibonacci(7)}")
+print(f"Число Фібоначчі для 14 дорівнює {fibonacci(14)}")
