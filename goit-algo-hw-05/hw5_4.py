0ac7bf66cd46dc1d8d67e1d37b2c0088d5c07ecc
nickkyiv <159835975+nickkyiv@users.noreply.github.com>
2024-03-22 19:10:18 +0000

    modified:   goit-algo-hw-05/hw5_4.py

--- goit-algo-hw-05/hw5_4.py
+++ goit-algo-hw-05/hw5_4.py
@@ -1,2 +1,81 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "After the command, please input name and phone."
+        except IndexError:
+            return "After the command, please input name."
+        except KeyError:
+            return "Please use a correct command. Type 'hello' for list of commands."
+    return inner
 
 
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+@input_error
+def add_contact(args, contacts): # Якщо таке ім’я вже є, ф-ція пропонує скористатися командою "change"
+    name, phone = args
+    if name in contacts:
+        return f"{name}'s phone is already in the list.\nPlease use 'change' command to update the phone."
+    else:
+        contacts[name] = phone
+        return "Contact added."
+
+@input_error
+def change_contact(args, contacts): # Тут ще більш удосконалено. Якщо користувач хоче змінювати неіснуючий контакт, бот пропонує натомість його створити 
+    name, phone = args
+    # if name in contacts:
+    contacts[name] = phone
+    return "Contact updated."
+    # else:
+    #     user_input = input(f"{name}'s phone is not in the list.\nType 'Y' to add {name}'s phone as a new contact. ")
+    #     if user_input.lower() == "y":
+    #         contacts[name] = phone
+    #         return f"{name}'s phone added to the list."
+    #     else:
+    #         return f""
+
+@input_error
+def show_contact(args, contacts):
+    name = args[0]
+    if name in contacts:
+        phone = contacts.get(name)
+        return f"{name}'s phone is {phone}"
+    else:
+        return f"{name}'s phone is not in the list."
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot! Type 'hello' to see the list of commands.")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            hello = "Please enter one of the following commands:\n"\
+            "- add [name] [phone number]\n"\
+            "- change [name] [phone number]\n"\
+            "- phone [name]\n"\
+            "- all\n"\
+            "- close or exit"
+            print(hello)
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_contact(args, contacts))
+        elif command == "all":
+            print(contacts)
+        else:
+            print("Invalid command. Type 'hello' to see the list of commands.")
+
+if __name__ == "__main__":
+    main()
